96e7ec7961758280dcdd60cb273dd6533b97e97a
Riya <149504570+Riyakhilari@users.noreply.github.com>
2024-11-27 23:11:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	s=s.trim;
+	const spaceIndex = s.Index = s.indexOf(' ');
+	 if (spaceIndex === -1) {
+        return s;
+    }
+	return s.slice(0, spaceIndex);
+
+	
 }
 
 // Do not change the code below
